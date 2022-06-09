5e18a81e2823270e956c85b61a259d981f3bf117
SohelTanbir <soheltanbir19@gmail.com>
2022-06-09 16:48:10 +0600

    design road and add events

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ window.addEventListener("load", function(){
     // all selectors
     const levelTitle = document.querySelector(".level-title");
     const level = document.querySelectorAll(".level");
-
+    const randomRoad = document.querySelector(".random-road");
+    const boundaryTop = document.querySelector(".boundary-top");
+    const boundaryBottom = document.querySelector(".boundary-bottom");
 
 
 
@@ -21,24 +23,40 @@ for (let i = 0; i < level.length; i++) {
     
 }
 
+boundaryBottom.addEventListener("mousedown",function(){
+    
+    // boundary touch event
+    boundaryTop.addEventListener("mouseover", detectBoundary);
+    boundaryBottom.addEventListener("mouseover", detectBoundary);
+})
+
+
+
+
 // add/remove border on level title
 const stopInterval = setInterval(borderToggle, 1000)
 function borderToggle(){
+   if(levelTitle){
     levelTitle.classList.toggle("border-2")
+   }
 };
 
-// detect selected game level
+// detect selected game level and redirect to level page
 function detectLevel(selectedLevel){
     if(selectedLevel.classList.contains("one")){
-        window.location.replace("level-one.html")
+      setTimeout(()=>   window.location.replace("level-one.html"), 500)
     }
     else if(selectedLevel.classList.contains("two")){
-        window.location.replace("level-two.html")
+        setTimeout(()=>  window.location.replace("level-two.html"), 500)
     }
     else if(selectedLevel.classList.contains("three")){
-        window.location.replace("level-three.html")
+        setTimeout(()=> window.location.replace("level-three.html"), 500)
     }
 }
 
+// detect boundary touch
+function detectBoundary(){
+    alert('touch');
+}
 
 })
