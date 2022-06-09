3597ae687a65fcc387195016e6e8c2f3929f9634
SohelTanbir <soheltanbir19@gmail.com>
2022-06-09 12:43:15 +0600

    created all level html files and added a function to detect user selected level and redirect to the level

--- js/main.js
+++ js/main.js
@@ -8,14 +8,36 @@ End Date: .........................
 */
 // when html document will be 100% loaded functin will be execute
 window.addEventListener("load", function(){
+    // all selectors
     const levelTitle = document.querySelector(".level-title");
+    const level = document.querySelectorAll(".level");
 
 
+
+
+// events occurse
+for (let i = 0; i < level.length; i++) {
+    level[i].addEventListener("click", ()=>detectLevel(level[i]))
+    
+}
 
 // add/remove border on level title
 const stopInterval = setInterval(borderToggle, 1000)
 function borderToggle(){
     levelTitle.classList.toggle("border-2")
+};
+
+// detect selected game level
+function detectLevel(selectedLevel){
+    if(selectedLevel.classList.contains("one")){
+        window.location.replace("level-one.html")
+    }
+    else if(selectedLevel.classList.contains("two")){
+        window.location.replace("level-two.html")
+    }
+    else if(selectedLevel.classList.contains("three")){
+        window.location.replace("level-three.html")
+    }
 }
 
 
